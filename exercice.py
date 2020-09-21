6c3fb7245221b91571745cefaef91eb3e9fc38ce
krab-z <70920725+krab-z@users.noreply.github.com>
2020-09-21 15:23:38 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -6,16 +6,37 @@ from typing import List
 
 
 def convert_to_absolute() -> float:
-    return 0.0
+    mon_nombre = input('Veuillez entrer un nombre:')
+    mon_nombre = float()
+    if mon_nombre<0:
+        mon_nombre = -mon_nombre    
+    return mon_nombre #ou abs(mon_nombre)
 
 
 def use_prefixes() -> List[str]:
     prefixes, suffixes = 'JKLMNOP', 'ack'
-
-    return [""]
+    resultat = []
+    for prefixe in prefixes:
+        nom = prefixe + suffixe
+        resultat.append(nom)
+    return resultat #[prefixe+suffixe for prefixe in prefixes]
 
 
 def prime_integer_summation() -> int:
+    nombrePremier = 0
+    nombre = 2
+    sommes = 0
+    while(nombrePremier <100)
+        estPremier= True
+        for i in range(1, nombre+1)
+            if(nombre%i ==0 and i!=1 and i!=nombre):
+                estPremier=False
+                break
+         if estPremier:
+             nombrePremier+=1
+             sommes+=nombre
+         nombre+=1
+    print(sommes)
     return 0
 
 
